9f5f93ae32121b8d9c9c908b0d719d873052ca0e
Roman Volosatovs <rvolosatovs@riseup.net>
2026-05-01 15:59:00 +0200

    build(nix): set env
    Signed-off-by: Roman Volosatovs <rvolosatovs@riseup.net>

--- flake.nix
+++ flake.nix
@@ -35,6 +35,17 @@
           ];
 
           env.LIBCLANG_PATH = "${pkgs.llvmPackages.libclang.lib}/lib";
+
+          # Point cargo at the vendored libkrun built by `make libkrun` so
+          # direct `cargo` invocations (clippy, check, build, rust-analyzer)
+          # work without going through the Makefile. PWD is the flake root
+          # when entering `nix develop` from this directory; `make libkrun`
+          # populates ./build/prefix.
+          shellHook = ''
+            export PKG_CONFIG_PATH="$PWD/build/prefix/lib/pkgconfig''${PKG_CONFIG_PATH:+:$PKG_CONFIG_PATH}"
+            export LIBRARY_PATH="$PWD/build/prefix/lib''${LIBRARY_PATH:+:$LIBRARY_PATH}"
+            export DYLD_FALLBACK_LIBRARY_PATH="$PWD/build/prefix/lib''${DYLD_FALLBACK_LIBRARY_PATH:+:$DYLD_FALLBACK_LIBRARY_PATH}"
+          '';
         } devShells;
     }
     // {
